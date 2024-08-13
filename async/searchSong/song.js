babeac42245b02496a661341e1a4f39c595c2b59
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-08-13 16:19:50 +0530

    TemSongs

---
+++ async/searchSong/song.js
@@ -0,0 +1,50 @@
+const apiURL = 'https://api.lyrics.ovh';
+
+
+
+async function searchSongs(term){
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    //console.log(res);
+    //console.log(data);
+
+    showData(data);
+}
+
+//searchSongs("never") -->  console
+
+// show the song list
+function showData(data){
+    result.innerHTML = `
+        <ul class="songs">
+            ${data.data.map(song => `
+                <li>
+                    <strong>${song.artist.name}</strong> - ${song.title}
+                    <button onclick="showLyrics('${song.artist.name}', '${song.title}')">Get Lyrics</button>
+                </li>
+            `).join('')}
+        </ul>
+    `;
+}
+
+// Show lyrics
+async function fetchLyrics(artist, title) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${title}`);
+    const data = await res.json();
+
+    //console.log(res);
+    //console.log(data);
+
+    showLyrics(data.lyrics);
+   
+}
+
+function showLyrics(lyricsText) {
+    lyrics.innerHTML = `<pre>${lyricsText} </pre>`;
+    
+}
+const result = document.getElementById('result');
+const lyrics = document.getElementById('lyrics');
+
+result.appendChild(data);
