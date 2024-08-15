5110bd2d5756189b87e907a64383c699730052c6
Pasan47 <148409682+Pasan47@users.noreply.github.com>
2024-08-15 08:37:48 +0530

    Delete Day 03 directory

--- Day 03/practical.js
+++
@@ -1,8 +0,0 @@
-// manipulating a div
-// without doing manually in HTML it is done by using JS
-
-const element = document.createElement('div');
-element.innerText = "Institute";
-element.innerHTML = `<p> Sanuri </p>`
-document.getElementById('Pace').append(element)
-document.getElementsByClassName("practical").append(element)
