a1a1992d00c7cd5dfbee758fa00934b3b2e0d0f1
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-08-15 23:18:50 +0530

    component

--- Day 05/classcomponent/src/App.js
+++ Day 05/classcomponent/src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-
 import React, { Component } from 'react';
-
+import './App.css';
+import Page from './page'; // Ensure the path is correct
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       message: "Hello World!",
-    }
+      name: "Sanuri" // Initial name prop value
+    };
   }
 
   handleClick = () => {
@@ -19,31 +17,30 @@ export default class App extends Component {
     }));
   }
 
-  componentDidMount (){
+  handleNameChange = () => {
+    this.setState((prevState) => ({
+      name: prevState.name === "Sanuri" ? "Updated Name" : "Sanuri" // Toggle between two names
+    }));
+  }
+
+  componentDidMount() {
     console.log("Component did mount");
   }
   
-  componentDidUpdate(prevProp,prevState){
+  componentDidUpdate(prevProps, prevState) {
     console.log("Component did update");
   }
-  
-
-
-
 
   render() {
     return (
       <div className='App'>
-        <header className='App-header'> 
-       <p> {this.state.message}</p>
-       <button onClick={this.handleClick}>Click Me</button>
-       </header>
+        <header className='App-header'>
+          <p>{this.state.message}</p>
+          <button onClick={this.handleClick}>Click Me</button>
+          <button onClick={this.handleNameChange}>Change Name</button> {/* Button to change the name */}
+          <Page name={this.state.name} /> {/* Pass the current name state to Page */}
+        </header>
       </div>
-    )
+    );
   }
 }
-
-
-
-
-
