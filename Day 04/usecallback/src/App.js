e7660b4b967a54dcf78696812e778a82b3a98d0b
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-08-15 16:24:32 +0530

    Day 04

--- Day 04/usecallback/src/App.js
+++ Day 04/usecallback/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
-import {useState} from "react";
+import {useState, useCallback} from "react";
 
 function App() {
 
 const [count, setCount] = useState(0);
 //function newFn(){}
-const bewFn = useCallback(
-  () => {  }, []);
+const newFn = useCallback(
+  () => {  }, [])
 
   return (
     <>
