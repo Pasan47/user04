339facdf88a1b9dd324602a99f45a60dea14d8d9
Pasan47 <148409682+Pasan47@users.noreply.github.com>
2024-08-15 08:36:28 +0530

    Day 03

--- Day 03/index.js
+++ Day 03/index.js
@@ -1,3 +1,26 @@
+//Variable declarations
+
 const name = "Sanuri"
 
 console.log(name)
+
+const age = "27"
+console.log(age)
+
+
+//function
+
+function displayName() {
+    console.log("displayFunation");
+}
+
+displayName();
+
+//Add two numbers
+
+function addNumbers( a,  b) {
+    console.log(a+b);
+    
+}
+
+addNumbers(2,3);
