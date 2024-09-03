60549b06348eed9e1c0e464b308d753d62caa345
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-09-03 16:16:14 +0530

    final commit

---
+++ Day 08/M-M/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const tute = require("./tutorial")
+const auth= require("./author")
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/tute",tute)
+
+app.use("/api/auth",tute)
+
+
+
+
+const port = 5000
+
+
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}`);
+})
