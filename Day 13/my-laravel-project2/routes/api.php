113c1364f9dfb1f24779a8e03a538c96ae6ea423
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-09-24 16:00:07 +0530

    Final commit

--- Day 13/my-laravel-project2/routes/api.php
+++ Day 13/my-laravel-project2/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\OrderController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,9 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//For books
 
 Route::get('/books',[BookController::class,'index']);
 Route::post('/books/addBook',[BookController::class,'create']);
 Route::get('/books/showBook/{id}',[BookController::class,'show']);
 Route::delete('/books/deleteBook/{id}',[BookController::class,'destroy']);
 Route::put('/books/editBook/{id}',[BookController::class,'edit']);
+
+//for Orders
+
+Route::get('/orders',[OrderController::class,'index']);
+Route::post('/orders/addOrder',[OrderController::class,'create']);
+Route::get('/orders/showOrder/{id}',[OrderController::class,'show']);
+Route::delete('/orders/deleteOrder/{id}',[OrderController::class,'destroy']);
+Route::put('/orders/editOrder/{id}',[OrderController::class,'edit']);
