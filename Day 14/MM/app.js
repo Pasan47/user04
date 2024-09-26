d1e188b44da74af52d5a57b20cd91ef9a0c81fd5
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-09-26 15:46:18 +0530

    final

--- Day 14/MM/app.js
+++ Day 14/MM/app.js
@@ -5,14 +5,20 @@ const post = require("./post")
 const order = require("./order")
 const user = require("./user")
 const auth = require("./student")
+const middle = require("./middleware/auth")
 const cookieParser = require("cookie-parser")
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+//for authentication
+
+app.use("/api/student",auth)
+
 //* For post
-app.use("/api/post",post)
+app.use("/api/post",middle, post)
 
 app.use(cookieParser())
 //* For Order
@@ -22,12 +28,15 @@ app.use("/api/order",order)
 //*For user
 app.use("/api/user",user)
 
-//for authentication
 
-app.use("/api/student",auth)
+
+
 const port = 5000
 
 
 app.listen(port,()=>{
     console.log(`App is running on port ${port}`);
 })
+
+
+
